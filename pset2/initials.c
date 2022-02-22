d47c32045c1723fb40bd13231070f246a3cdb05a
alvintwng <72246214+alvintwng@users.noreply.github.com>
2022-02-22 21:06:53 +0800

    revised to move cs50 function to here
    line 16 - added `typedef char* string;`  for `string`
    line 47 - added cs50's `string GetString(void)` to here

--- pset2/initials.c
+++ pset2/initials.c
@@ -1,23 +1,25 @@
 /* initials.c
- * 
+ * 2022 Feb 20, revised to move cs50 function to here.
+ *
  * a program that prompts user for their name
  * and then outputs their initials in uppercase with no spaces or periods.
  * Assume that the user's input will contain only letters (uppercase
  * and/or lowercase) plus single spaces between words.
  *
  * to check the correctness:
- * check50 2015.fall.pset2.initials initials.c
- *
  */
 
-#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
+typedef char* string;   /* from cs50.h */
+string GetString(void); /* prototype */
+
 int main(void)
 {
     //get line of text
+    printf("Name please: ");    
     string s = GetString();
 
     //error checking
@@ -34,14 +36,88 @@ int main(void)
             }
         }
         printf("\n");
-    }
-    
+    }    
 }
-/*OUTPUT
-~/workspace/pset2 $ ./initials
-Zamyla Chan
+
+
+#include <limits.h>
+#include <stdlib.h>
+
+/* from cs50.c */
+string GetString(void)
+{
+    // growable buffer for chars
+    string buffer = NULL;
+
+    // capacity of buffer
+    unsigned int capacity = 0;
+
+    // number of chars actually in buffer
+    unsigned int n = 0;
+
+    // character read or EOF
+    int c;
+
+    // iteratively get chars from standard input
+    while ((c = fgetc(stdin)) != '\n' && c != EOF)
+    {
+        // grow buffer if necessary
+        if (n + 1 > capacity)
+        {
+            // determine new capacity: start at 32 then double
+            if (capacity == 0)
+            {
+                capacity = 32;
+            }
+            else if (capacity <= (UINT_MAX / 2))
+            {
+                capacity *= 2;
+            }
+            else
+            {
+                free(buffer);
+                return NULL;
+            }
+
+            // extend buffer's capacity
+            string temp = realloc(buffer, capacity * sizeof(char));
+            if (temp == NULL)
+            {
+                free(buffer);
+                return NULL;
+            }
+            buffer = temp;
+        }
+
+        // append current character to buffer
+        buffer[n++] = c;
+    }
+
+    // return NULL if user provided no input
+    if (n == 0 && c == EOF)
+    {
+        return NULL;
+    }
+
+    // minimize buffer
+    string minimal = malloc((n + 1) * sizeof(char));
+    strncpy(minimal, buffer, n);
+    free(buffer);
+
+    // terminate string
+    minimal[n] = '\0';
+
+    // return string
+    return minimal;
+}
+
+/*
+antw@Mac-mini c % make initials
+cc     initials.c   -o initials
+antw@Mac-mini c % ./initials
+Name please: Zamyla Chan
 ZC
-~/workspace/pset2 $ ./initials
-robert thomas bowden
+antw@Mac-mini c % ./initials
+Name please: robert thomas bowden
 RTB
 */
